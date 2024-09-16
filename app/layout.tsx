6918b7dd415b16b9d8a751514f276e7fb3a98928
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-09-16 17:59:42 +0545

    Next js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Provider from "./util/Provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+    <Provider>
+
         {children}
+    </Provider>
+
+
       </body>
     </html>
   );
---
+++ app/productlist/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+
+// async function productList() {
+//     let data = await fetch('https://dummyjson.com/products');
+//     data = await data.json();
+//     return data.products
+// };
+
+const ProductPage = () => {
+  const [product, setProduct] = useState([]);
+
+  const {data, isLoading} = useQuery({
+    queryKey: ['product'],
+    queryFn: () => fetch('https://dummyjson.com/products').then((res) => 
+    res.json()),
+
+  });
+
+
+
+  useEffect(() => {
+    if(data) {
+        setProduct(data?.products);
+    }
+  }, [data]);
+
+  if(isLoading) return <p>Data is loading....</p>
+
+  console.log(data);
+
+    // useEffect(() => {
+    //     const fetchData = async() => {
+
+    //         let data = await fetch('https://dummyjson.com/products');
+    //         data = await data.json();
+    //         console.log(data);
+    //         setProduct(data.products);
+    //     }
+    //     fetchData();
+
+        // let data = await fetch('https://dummyjson.com/products');
+        // data = await data.json();
+        // console.log(data);
+    // }, []);
+
+    // let products = await productList();
+    // console.log(products);
+
+  return (
+ 
+    <>
+    
+    <div className=' flex flex-col items-center'>
+        Product List
+        {product.map((p) => (
+
+            <Card className='w-1/2 my-2'>
+            <CardHeader>
+              <CardTitle>{p.title}</CardTitle>
+              <CardDescription>{p.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className='font-semibold text-cyan-700'>{p.price}</p>
+            </CardContent>
+          </Card>
+        ))}
+    </div>
+
+
+    </>
+  )
+}
+
+export default ProductPage;
